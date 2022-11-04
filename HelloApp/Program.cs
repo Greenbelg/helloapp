b8c72bae613672b1a4ecc3c5c17014482b540c5f
black <black@example.com>
2022-11-04 18:06:11 +0500

    Create GreetBlack

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -7,6 +7,11 @@
             Console.WriteLine("hello everyone");
         }
 
+        static void GreetBlack()
+        {
+            Console.WriteLine("hello black");
+        }
+        
         static void GreetWhite()
         {
             Console.WriteLine("hello white");
